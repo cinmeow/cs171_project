8e95305f1e5bb2f2c5bcd98f77b1ad9c348e3e6c
Erica Ma <ericama.1996@gmail.com>
2023-11-19 11:09:07 -0500

    added csv and to main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
 console.log("let's get started!")
+// initiate global variables
+
+
+// load data with promises
+let promises = [
+    d3.csv("data/Travel_data.csv", (row) => {
+        console.log(row)
+        row.Year = parseInt(row.Year);
+        return row
+    })
+];
+Promise.all(promises)
+    .then(function (data) {
+        initMainPage(data)
+    })
+    .catch(function (err) {
+        console.log(err)
+    });
+
+// initMainPage
+function initMainPage(dataArray) {
+    console.log(dataArray)
+}
