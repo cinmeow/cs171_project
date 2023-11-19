623b73959687c6370fd69f92a394146798fe9fd4
Erica Ma <ericama.1996@gmail.com>
2023-11-19 15:20:26 -0500

    edit travel data csv and formatted data in js

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
 console.log("let's get started!")
 // initiate global variables
-
+let michelinCountry = new Set()
 
 // load data with promises
 let promises = [
     d3.csv("data/Travel_data.csv", (row) => {
-        console.log(row)
         row.Year = parseInt(row.Year);
+        row.avgLengthStay =+ row.avgLengthStay
         return row
-    })
+    }),
+    d3.csv("data/michelin.csv")
 ];
 Promise.all(promises)
     .then(function (data) {
@@ -20,5 +21,11 @@ Promise.all(promises)
 
 // initMainPage
 function initMainPage(dataArray) {
-    console.log(dataArray)
+    console.log(dataArray[0]);
+    console.log(dataArray[1])
+    dataArray[1].forEach((d) => {
+      michelinCountry.add(d.Country)
+    })
+    console.log(dataArray[0].map((d) => {return d.avgLengthStay}))
 }
+console.log("meow", michelinCountry)
