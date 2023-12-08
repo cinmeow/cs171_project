2d51a51617fcdc250912cac236c8fc200cee5558
Erica Ma <ericama.1996@gmail.com>
2023-12-07 23:31:16 -0500

    added animation slide (html, css, js additions)

---
+++ js/estonian-restaurant.js
@@ -0,0 +1,78 @@
+const numPeopleEstonia = 50;
+const restaurantWidthEstonia = document.getElementById("estonia-restaurant").getBoundingClientRect().width;
+const restaurantHeightEstonia = 500;
+const doorPositionEstonia = { x: 250, y: 175 };
+let counterEstonia = 0
+// Create the SVG container using D3
+const svgEstonia = d3.select("#estonia-restaurant")
+    .append("svg")
+    .attr("width", restaurantWidthEstonia)
+    .attr("height", restaurantHeightEstonia);
+
+svgEstonia.append("image")
+    .attr("xlink:href", "img/Est-rest 2.png") // Replace with the actual path or URL
+    .attr("width", restaurantWidthEstonia)
+    .attr("height", restaurantHeightEstonia)
+    .attr("x", 50)
+    .attr("y", -50);
+
+// Function to add a person to the SVG
+function addPerson_Estonia() {
+    return svgEstonia.append("image")
+        .attr("class", "personEstonia")
+        .attr("xlink:href", "img/walking_estonia.png")
+        .attr("width", restaurantWidthEstonia)
+        .attr("height", restaurantHeightEstonia)
+        .attr("x", -50) // Initial x position outside the restaurant
+        .attr("y", restaurantHeightEstonia) // Initial y position (center of the restaurant)
+        .attr("opacity", 1);
+}
+
+// Function to animate a person walking to the door and disappearing
+// Function to animate a person walking to the door and disappearing
+function animatePersonEstonia(person) {
+    person.transition()
+        .duration(10000)
+        .attr("x", doorPositionEstonia.x)
+        .attr("y", doorPositionEstonia.y)
+        .on("start", function () {
+            // Set the starting opacity to 1 (fully opaque)
+            d3.select(this).attr("opacity", 1);
+        })
+        .on("end", function () {
+            // Fade out the person
+            d3.select(this).transition().duration(300).attr("opacity", 0);
+
+            // Remove the person from the DOM after the animation ends
+            d3.select(this).transition().delay(500).remove();
+        });
+}
+
+
+// Function to start animations for multiple people with staggered start times
+function animatePeopleEstonia() {
+    for (let i = 0; i < numPeopleEstonia; i++) {
+        const person = addPerson_Estonia();
+        // Stagger the start times using setTimeout
+        setTimeout(() => animatePersonEstonia(person), i * 5000);
+    }
+}
+
+// Call the animatePeople function
+animatePeopleEstonia();
+
+// Call the animatePeople function at regular intervals to create a continuous loop
+setInterval(animatePeopleEstonia, 55000); // 11000 milliseconds (10s for animation + 1s delay before starting the next animation)
+
+// Function to update the counter
+function updateCounterEstonia() {
+    if(counterEstonia < 51){
+        counterEstonia++;
+        d3.select("#counter-boxEstonia").text(counterEstonia);
+    }
+}
+
+// Update the counter every second (adjust the interval as needed)
+setTimeout(function () {
+    setInterval(updateCounterEstonia, 200);
+}, 9000);
---
+++ js/french-restaurant.js
@@ -0,0 +1,79 @@
+const numPeople = 500;
+const restaurantWidth = document.getElementById("french-restaurant").getBoundingClientRect().width;
+const restaurantHeight = 500;
+const doorPosition = { x: 180, y: 175 };
+let counter = 0
+
+// Create the SVG container using D3
+const svg = d3.select("#french-restaurant")
+    .append("svg")
+    .attr("width", restaurantWidth)
+    .attr("height", restaurantHeight);
+
+svg.append("image")
+    .attr("xlink:href", "img/French-rest.png") // Replace with the actual path or URL
+    .attr("width", restaurantWidth)
+    .attr("height", restaurantHeight)
+    .attr("x", -60)
+    .attr("y", -50);
+
+// Function to add a person to the SVG
+function addPerson() {
+    return svg.append("image")
+        .attr("class", "person")
+        .attr("xlink:href", "img/walking_france.png")
+        .attr("width", restaurantWidth)
+        .attr("height", restaurantHeight)
+        .attr("x", restaurantWidth) // Initial x position outside the restaurant
+        .attr("y", restaurantHeight) // Initial y position (center of the restaurant)
+        .attr("opacity", 1);
+}
+
+// Function to animate a person walking to the door and disappearing
+// Function to animate a person walking to the door and disappearing
+function animatePerson(person) {
+    person.transition()
+        .duration(10000)
+        .attr("x", doorPosition.x)
+        .attr("y", doorPosition.y)
+        .on("start", function () {
+            // Set the starting opacity to 1 (fully opaque)
+            d3.select(this).attr("opacity", 1);
+        })
+        .on("end", function () {
+            // Fade out the person
+            d3.select(this).transition().duration(300).attr("opacity", 0);
+
+            // Remove the person from the DOM after the animation ends
+            d3.select(this).transition().delay(500).remove();
+        });
+}
+
+
+// Function to start animations for multiple people with staggered start times
+function animatePeople() {
+    for (let i = 0; i < numPeople; i++) {
+        const person = addPerson();
+        // Stagger the start times using setTimeout
+        setTimeout(() => animatePerson(person), i * 1000);
+    }
+}
+
+// Call the animatePeople function
+animatePeople();
+
+// Call the animatePeople function at regular intervals to create a continuous loop
+setInterval(animatePeople, 83000); // 11000 milliseconds (10s for animation + 1s delay before starting the next animation)
+
+// Function to update the counter
+function updateCounter() {
+    if(counter < 689){
+        counter++;
+        d3.select("#counter-boxFrance").text(counter);
+    }
+}
+
+// Update the counter every second (adjust the interval as needed)
+setTimeout(function () {
+    setInterval(updateCounter, 50);
+}, 9000);
